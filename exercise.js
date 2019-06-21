2876fbe8a2099e7d61b576a33c9c6bb55a486409
Alexander G. Yang <spark8402@rogers.com>
2019-06-20 21:51:15 -0400

    assignment done

---
+++ exercise.js
@@ -0,0 +1,21 @@
+const tasks = ["laundry", "shovel driveway", "grocery shopping", "check email", "daydream", "phone parents", "fill prescription", "exercise", "take out recycling", "check the weather"];
+
+console.log("Sunday To Do List:");
+
+for(let i = 0; i < tasks.length; i++) {
+    
+    console.log(`${[i+1]}: ${tasks[i]}`);
+    
+}
+
+// Sunday To Do List:
+// 1: laundry
+// 2: shovel driveway
+// 3: grocery shopping
+// 4: check email
+// 5: daydream
+// 6: phone parents
+// 7: fill prescription
+// 8: exercise
+// 9: take out recycling
+// 10: check the weather
